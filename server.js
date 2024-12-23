656dedb02d5ad2e190b1fd79a93508c3c6543772
bfoster4 <beaudfoster@gmail.com>
2024-12-23 15:14:09 -0600

    Added dotenv, variables, mongo connection

--- server.js
+++ server.js
@@ -1,5 +1,19 @@
+//  require dependencies
 const express = require('express')
 const app = express()
 const cors = require('cors')
 const MongoClient = require('mongodb').MongoClient
 require('dotenv').config
+
+// declare variables
+let db,
+    dbConnectionString = process.env.DB_STRING,
+    dbName = 'sample_mflix',
+    collection
+
+MongoClient.connect(dbConnectionString)
+    .then(client => {
+        console.log('Connected to the Database')
+        db = client.db(dbName)
+        collection = db.collection('movies')
+    })
